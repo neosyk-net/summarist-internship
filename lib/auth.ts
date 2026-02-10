4a75600041ca47d6b7c49c37223a70936b082d9a
neosyk-net <runerdycastillo@gmail.com>
2026-02-10 12:52:27 -0500

    Setup Firebase auth config and listener

---
+++ lib/auth.ts
@@ -0,0 +1,18 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "./firebase";
+
+export function signUp(email: string, password: string) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function signIn(email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function logOut() {
+  return signOut(auth);
+}
